da4e81e0a1e2b373bcce5d5f635fa33f9d0e3be8
obsfx <balandiomer@gmail.com>
2020-09-24 05:37:16 +0300

    quick fix on blockquote

--- src/parser/components/Blockquote.ts
+++ src/parser/components/Blockquote.ts
@@ -25,6 +25,25 @@ export default abstract class Blockquote {
 
         let inlineContext: string = context.substring(seqs[0].idx, seqs[seqs.length - 1].idx);
 
+        let lines: string[] = inlineContext.split('\n');
+
+        for (let i: number = 0; i < lines.length; i++) {
+            let line: string[] = lines[i].split('');
+            let anychar: boolean = false;
+
+            for (let j: number = 0; j < line.length; j++) {
+                if (line[j] == '>' && !anychar) {
+                    line[j] = '';
+                }
+
+                if (line[j] != ' ') anychar = true;
+            }
+
+            lines[i] = line.join('');
+        }
+
+        inlineContext = lines.join('\n');
+
         let p: Element = new Element('p');
         let InlineParser: Inline = new Inline(inlineContext, p);
         let inlineParseRes: t_inlineParseResult = InlineParser.parse();
